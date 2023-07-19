1f128b248b6605a5518728074d03b3793dbfb733
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-19 18:34:48 +0900

    メモ一覧画面のprovider設定

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,70 +1,19 @@
 /// メモ一覧画面用のWidget
 import 'package:flutter/material.dart';
-import 'package:flutter_task_3/Model.dart';
 import 'package:flutter_task_3/screen3.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import 'memo.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required String titleName});
-
-  void handleOkButtonTap() {
-    //削除ボタンではいが押されたときの処理
-    print('メモタイトル削除');
-  }
-
-  void showConfirmationDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext dialogContext) {
-        return AlertDialog(
-          contentPadding: EdgeInsets.zero,
-          actionsPadding: const EdgeInsets.all(50),
-          actionsAlignment: MainAxisAlignment.spaceEvenly,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(40.0),
-          ),
-          content: Container(
-            height: 140,
-            alignment: Alignment.bottomCenter,
-            child: const Text(
-              '「メモのタイトル」を\n削除してもよろしいですか？',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 13,
-              ),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                context.pop(); // ダイアログを閉じる
-              },
-              child: const Text('いいえ'),
-            ),
-            TextButton(
-              onPressed: () {
-                handleOkButtonTap(); // OKボタンの処理を実行
-                context.pop(); // ダイアログを閉じる
-              },
-              child: const Text('はい'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Model> dummyDataList = [
-    Model(id: '1', title: 'title1'),
-    Model(id: '2', title: 'title2'),
-    Model(id: '3', title: 'title3'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -94,10 +43,10 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.all(8.0),
             child: ListView.builder(
               shrinkWrap: true,
-              itemCount: dummyDataList.length,
+              itemCount: context.watch<Memo>().list.length,
               itemBuilder: (context, index) {
                 //以下の記載でModelクラスの単体のデータ取得が可能
-                final Model modelData = dummyDataList[index];
+                final modelData = context.watch<Memo>().list[index];
                 return Padding(
                   padding: const EdgeInsets.only(bottom: 20),
                   child: Container(
@@ -123,8 +72,10 @@ class _HomeScreenState extends State<HomeScreen> {
                               icon: const Icon(Icons.delete),
                               iconSize: 35, // アイコンのサイズを指定
                               onPressed: () {
-                                widget.showConfirmationDialog(context);
-                                // ボタンがタップされた時の処理
+                                // 削除ボタンがタップされた時の処理
+                                context
+                                    .read<Memo>()
+                                    .removeItem(context, modelData.id);
                               },
                             ),
                           ),
